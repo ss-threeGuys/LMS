5e8a5ae2c65583dae37c26dbaa3612b4afc01c87
ace <allisterg@gmail.com>
2020-03-05 13:08:36 -0500

    add basic error handling

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ app.use("/admin", controllers);
 app.listen(3000);
 console.log("server running on port 3000");
 
+app.use((err, req, res, next) => {
+
+    if (res.headersSent) {
+        return next(err)
+    }
+
+    if (err.status) {
+        return res.status(err.status).json(err);
+    }
+    res.status(500).json(err);
+});
+
 mongoose.connect("mongodb://localhost/library");
 
