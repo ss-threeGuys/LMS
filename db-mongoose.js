f28dc4d1fd651ab3f5dad1b91730886b55525b3c
ace <allisterg@gmail.com>
2020-03-09 14:36:25 -0400

    moved db connection and exception handler out of index

---
+++ db-mongoose.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+// Build the connection string 
+const dbURI = 'mongodb+srv://use:user@cluster0-xarex.mongodb.net/library?retryWrites=true&w=majority'; 
+ 
+// Create the database connection 
+mongoose.connect(dbURI, { useNewUrlParser: true , useUnifiedTopology: true })
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose default connection open to ' + dbURI);
+}); 
+  
+// If the connection throws an error
+mongoose.connection.on('error', (err) => { 
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected',  () => { 
+  console.log('Mongoose default connection disconnected'); 
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', () => {   
+  mongoose.connection.close(() => { 
+    console.log('Mongoose default connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+}); 
+
+
+
+
+
+
