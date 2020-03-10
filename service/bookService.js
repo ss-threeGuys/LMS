1c531e4a179866e200aa4dd08245c0febf561543
ace <allisterg@gmail.com>
2020-03-10 17:12:05 -0400

    transactions bookservice

--- service/bookService.js
+++ service/bookService.js
@@ -1,4 +1,4 @@
-
+const mongoose = require('mongoose')
 const Book = require('../models/Book');
 const Author = require('../models/Author');
 const Genre = require('../models/Genre');
@@ -14,7 +14,7 @@ function checkBookAuthors(book) {
     } else {
         return [];
     }
-    
+
 }
 
 function checkBookGenres(book) {
@@ -41,29 +41,41 @@ function createBook(book) {
     if (!book.genres) {
         book.genres = [];
     }
-    
-    return checkBookAuthors(book)
-        .then(authors => {
-            if (authors.length) {
-                book.authors = book.authors.filter((author, index) => authors[index]);
-            }
-            return checkBookGenres(book)
-        })
-        .then(genres => {
-            if (genres.length) {
-                book.genres = book.genres.filter((genre, index) => genres[index]);
-            }
 
-            return checkPublisher(book)
+    return mongoose.startSession()
+        .then(_session => {
+            session = _session;
+            session.startTransaction();
+            return checkBookAuthors(book)
+                .then(authors => {
+                    if (authors.length) {
+                        book.authors = book.authors.filter((author, index) => authors[index]);
+                    }
+                    return checkBookGenres(book)
+                })
+                .then(genres => {
+                    if (genres.length) {
+                        book.genres = book.genres.filter((genre, index) => genres[index]);
+                    }
+
+                    return checkPublisher(book)
+                })
+                .then(publisherExists => {
+                    if (!publisherExists) {
+                        book.publisher = null;
+                    }
+
+                    return Book.create(book)
+                })
+                .then(book => {
+                    session.commitTransaction();
+                    return book;
+                })
+                .catch(err => {
+                    session.abortTransaction();
+                    throw err;
+                })
         })
-        .then(publisherExists => {
-            if (!publisherExists) {
-                book.publisher = null;
-            }
-            return Book.create(book)
-        })
-        .then(book => book)
-        .catch(err => console.error(err))
 }
 
 function updateBook(book) {
@@ -74,29 +86,40 @@ function updateBook(book) {
     if (!book.genres) {
         book.genres = [];
     }
-    
-    return checkBookAuthors(book)
-        .then(authors => {
-            if (authors.length) {
-                book.authors = book.authors.filter((author, index) => authors[index]);
-            }
 
-            return checkBookGenres(book)
+    return mongoose.startSession()
+        .then(_session => {
+            session = _session;
+            session.startTransaction();
+            return checkBookAuthors(book)
+                .then(authors => {
+                    if (authors.length) {
+                        book.authors = book.authors.filter((author, index) => authors[index]);
+                    }
+
+                    return checkBookGenres(book)
+                })
+                .then(genres => {
+                    if (genres.length) {
+                        book.genres = book.genres.filter((genre, index) => genres[index]);
+                    }
+                    return checkPublisher(book)
+                })
+                .then(publisherExists => {
+                    if (!publisherExists) {
+                        book.publisher = null;
+                    }
+
+                    return Book.findByIdAndUpdate(book._id, { title: book.title, authors: book.authors, genres: book.genres, publisher: book.publisher })
+                })
+                .then(() => {
+                    session.commitTransaction();
+                })
+                .catch(err => {
+                    session.abortTransaction();
+                    throw err;
+                });
         })
-        .then(genres => {
-            if (genres.length) {
-                book.genres = book.genres.filter((genre, index) => genres[index]);
-            }
-            return checkPublisher(book)
-        })
-        .then(publisherExists => {
-            if (!publisherExists) {
-                book.publisher = null;
-            }
-            console.log("in checkpublisher callback" + JSON.stringify(book));
-            return Book.findByIdAndUpdate(book._id, { title: book.title, authors: book.authors, genres: book.genres, publisher: book.publisher })
-        })
-        .catch(err => console.error(err))
 }
 
 function deleteBook(id) {
